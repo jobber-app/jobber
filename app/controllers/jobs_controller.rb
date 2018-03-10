d4a6f518b39ecf3ec47078c9fd303baa0d024b96
Mikey <31766607+andmikey@users.noreply.github.com>
2018-03-10 17:59:48 +0000

    Add CRUD methods to job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -7,21 +7,38 @@ class JobsController < ApplicationController
   end
   
   def new
-    
+    @job = Job.new
   end
 
   def create
-    @job = Job.new(params[:job])
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to @job
+    else
+      render 'new'
   end
 
   def edit
+    @job = Job.find(params[:id])
   end
 
   def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to @job
+    else
+      render 'edit'
   end
 
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+  end
+  
   private
   def job_params
+    params.requre(:job).permit(:title, :postlink, :posttext, :applydate, :location,
+                               :status, :coverletter, :contactdetails, :additionalinfo)
   end
 
   def logged_in_user
