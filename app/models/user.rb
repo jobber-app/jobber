91d556e89279ea74a45804fe7b929dad8ab563f7
Mikey <31766607+andmikey@users.noreply.github.com>
2018-03-10 13:37:53 +0000

    Add email validation regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :name, :presence: true, length: {maximum: 50}
-  calidates :email, :presence: true, length: {maximum: 255}
+  validates :email, :presence: true, length: {maximum: 255}
+  # Regex to validate email from Rails tutorial bookg
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
