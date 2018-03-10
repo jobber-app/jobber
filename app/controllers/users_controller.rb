22d71f8c3a1f4455b5ce41aaeb39548332e14275
Mikey <31766607+andmikey@users.noreply.github.com>
2018-03-10 15:11:51 +0000

    Restrict access to show to only logged-in user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update, :show]
   
   def show
     @user = User.find(params[:id])
