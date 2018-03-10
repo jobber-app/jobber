2dd80ec78848a31e68f165ead056b4c22e33299c
Mikey <31766607+andmikey@users.noreply.github.com>
2018-03-10 15:31:36 +0000

    Add current user checking

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,12 +44,13 @@ class UsersController < ApplicationController
   def logged_in_user
     unless logged_in?
       flash[:danger] = "Please log in"
-      redirect_to login_url
+      redirect_to login_url unless current_user?(@user)
     end
   end
 
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless @user == current_user
+  end
   
 end
