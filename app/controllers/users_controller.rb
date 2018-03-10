de78fa593f69a9caa2abb1932c8f9d78d278c444
Mikey <31766607+andmikey@users.noreply.github.com>
2018-03-10 15:20:49 +0000

    Add check if user id is correct for auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update, :show]
+  before_action :correct_user, only: [:edit, :update, :show]
   
   def show
     @user = User.find(params[:id])
@@ -46,5 +47,9 @@ class UsersController < ApplicationController
       redirect_to login_url
     end
   end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
   
 end
