88178a6869f08674dd33b00ef04977485e548156
Mikey <31766607+andmikey@users.noreply.github.com>
2018-03-10 14:13:34 +0000

    Fix sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions:#destroy'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
