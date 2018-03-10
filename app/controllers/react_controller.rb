9a6286b367d82d761c5ee6352eb96523cd14d51c
Dylan Thinnes <dylan.j.thinnes@gmail.com>
2018-03-10 19:24:40 +0000

    Add Static & React controller, Add custom routes
    /app leads to the namespace managed by React controller
    /home, /about lead to static parts of the site, handled by Static controller
    / redirects to /home

---
+++ app/controllers/react_controller.rb
@@ -0,0 +1,4 @@
+class ReactController < ApplicationController
+    def index
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # get 'users/new'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/home', to: 'static#home'
+  get '/about', to: 'static#about'
+  get '/app', to: 'react#index'
+  root 'static#home'
   resources :users
 end
