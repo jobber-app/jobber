b3ae569222b11a174359426bb1455384dd828572
Mikey <31766607+andmikey@users.noreply.github.com>
2018-03-10 14:12:50 +0000

    Add Sessions for login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  # get 'users/new'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions:#destroy'
   resources :users
 end
