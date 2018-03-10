2e78bb129f8c419f58b65e7762c7242c8b010332
Mikey <31766607+andmikey@users.noreply.github.com>
2018-03-10 13:41:25 +0000

    Reformat user (again)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  validates :name, :presence: true, length: {maximum: 50}
-  VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, :presence: true, length: {maximum: 255},
-            format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+
 end
