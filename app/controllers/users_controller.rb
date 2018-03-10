8e9008b3090b7017319ee3cba2693e7b92f57db3
Mikey <31766607+andmikey@users.noreply.github.com>
2018-03-10 14:04:31 +0000

    Add new user success code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Stuff
+      flash[:success] = "Welcome to jobber!"
+      redirect_to @user
     else
       render 'new'
     end
