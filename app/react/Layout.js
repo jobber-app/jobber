d871135887e5bed93975ad4897e075107ac6d994
Dylan Thinnes <dylan.j.thinnes@gmail.com>
2018-03-11 01:20:24 +0000

    Finalize component shelves

--- app/react/Layout.js
+++ app/react/Layout.js
@@ -1,16 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import List from "./List";
 
 export default class Layout extends React.Component {
     constructor () {
         super()
     }
 
+    get appsActive () { return this.props.location.pathname === "/applications" }
+    get cvsActive  () { return this.props.location.pathname === "/cvs" }
+
     render () {
+        console.log(this.props.location, this.props.location.pathname === "/applications");
         return (
-<div class="container-fluid no-gutters">
-    <div class="row" id="toolbar">
-        <div class={ "col-2 page-button btn btn-lg m-1" } id="applications">Applications</div>
-        <div class={ "col-2 page-button btn btn-lg m-1" } id="cvs">CVs</div>
+<div className="container-fluid no-gutters">
+    <div className="row" id="toolbar">
+        <div className={ "col-2 rounded-0 page-button btn btn-lg ml-2 " + (this.appsActive ? "font-weight-bold active" : "") } id="applications"><Link to="/applications">Applications</Link></div>
+        <div className={ "col-2 rounded-0 page-button btn btn-lg ml-2 " + (this.cvsActive  ? "font-weight-bold active" : "") } id="cvs"><Link to="/cvs">CVs</Link></div>
+    </div>
+    <div className="row" id="page">
+        <div className="col-2" id="list">a</div>
+        <div className="col" id="details">b</div>
     </div>
 </div>
         );
--- app/react/main.js
+++ app/react/main.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Layout from "./Layout";
+import { HashRouter, Route } from "react-router-dom";
+import store from "./store";
 
 var root = document.getElementById("react-app");
-ReactDOM.render(<Layout />, root);
+ReactDOM.render(<HashRouter><Route path="/" component={ Layout }/></HashRouter>, root);
