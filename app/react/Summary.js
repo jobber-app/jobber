a7d5c7709413efcc9e3caa6e908b6ee513e4001a
Dylan Thinnes <dylan.j.thinnes@gmail.com>
2018-03-13 21:46:32 +0000

    Fix summary, Show jobs in stages

--- app/react/Summary.js
+++ app/react/Summary.js
@@ -3,8 +3,16 @@ import { observer } from "mobx-react";
 import { Route, Switch } from "react-router-dom";
 import store from "./store";
 
-@observer class Summary extends React.Component {
+export default @observer class Summary extends React.Component {
     render () {
-        return "Summary:";
+        return (
+<div>
+    <h4>Your Summary:</h4>
+    <p>You have { store.jobs.filter(job => job.status === undefined || job.status === 0).length } jobs that are in the planning stage.</p>
+    <p>You have { store.jobs.filter(job => job.status === 1).length } jobs that are in the application stage.</p>
+    <p>You have { store.jobs.filter(job => job.status === 2).length } jobs that are in the interview stage.</p>
+    <p>You have { store.jobs.filter(job => job.status === 3).length } jobs that are completed.</p>
+</div>
+        );
     }
 }
