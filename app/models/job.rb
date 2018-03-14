473de7c7ddfa7f352b9dc3036c33c6be81859016
Mikey <31766607+andmikey@users.noreply.github.com>
2018-03-14 21:55:23 +0000

    Remove attr_accessible
    See:
    https://stackoverflow.com/questions/23437830/undefined-method-attr-accessible

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,4 @@
 class Job < ApplicationRecord
-  attr_accessible :title, :postlink, :posttext, :applydate, :location,
-                  :status, :coverletter, :contactdetails, :additionalinfo
   belongs_to :user
   default_scope -> {order(created_at: :desc)}
   validates :user_id, presence: true
