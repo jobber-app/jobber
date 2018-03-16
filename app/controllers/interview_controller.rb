3ec8cb3034b6a64ac287303ad2708f7e96c2a6ac
Mikey <31766607+andmikey@users.noreply.github.com>
2018-03-16 09:56:02 +0000

    Add Interview model, controller

--- app/controllers/interview_controller.rb
+++ app/controllers/interview_controller.rb
@@ -1,2 +1,46 @@
 class InterviewController < ApplicationController
+  before_action :logged_in_user, only: [:create, :edit, :update, :show, :destroy]
+  before_action :correct_user, only: [:edit, :update, :show, :destroy]
+
+  def show
+    @interview = Interview.find(params[:id])
+    render json: @interview
+  end
+
+  def new
+    @interview = Interview.new
+  end
+
+  def create
+    @interview = Interview.new(interview_params)
+    if @interview.save
+      redirect_to @interview
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @interview = Interview.find(params[:id])
+  end
+
+  def update
+    @interview = Interview.find(params[:id])
+    if @interview.update(interview_params)
+      redirect_to @interview
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @interview = Interview.find(params[:id])
+    @interview.destroy
+  end
+
+  private
+  def interview_params
+    params.require(:interview).permit(:date, :notes)
+  end
+    
 end
--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,2 +1,5 @@
 class Interview < ApplicationRecord
+  belongs_to :job
+  default_scope -> {order(date: :desc)}
+  validates :job_id, presence: true
 end
