be347d6951370bb459405b68c13f4e3ce6ad57f8
Mikey <31766607+andmikey@users.noreply.github.com>
2018-03-21 14:43:31 +0000

    Add database seeds for user test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Run `rake db:seed`
+
+user1 = User.create(name: 'Foo Bar', email: 'example@example.com', password: 'Foo Bar Baz')
+
+user2 = User.create(name: 'Spam Baz', email: 'ham@spam.com', password: 'Foo Foo Foo')
+
+# Fill out your jobs here....
+# job11 = user1.jobs.create( ... )
+# job12 = user2.jobs.create( ... )
+
+# job21 = user2.jobs.create( ... )
+# job22 = user2.jobs.create( ... )
+
+
+
