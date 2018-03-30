37e69d6b79c35eccfa66d6704cf959c183bc433e
Dylan Thinnes <dylan.j.thinnes@gmail.com>
2018-03-30 16:54:56 +0100

    Rewrite Editable.MDate to be fast and lightweight

--- app/react/Editable.js
+++ app/react/Editable.js
@@ -1,6 +1,6 @@
 import React from "react";
 import store from "./store";
-import { reaction } from "mobx";
+import { observable, reaction } from "mobx";
 import { observer } from "mobx-react";
 import Modal from "./Modal";
 import { CVCreator } from "./ResourceCreator";
@@ -138,8 +138,9 @@ Editable.defaultProps = {
             return (
 <input ref={ input => this.input = input } 
        type="text"
-       class={ "form-control " + this.largeClass } 
-       value={ this.state.data } onBlur={ this.cancel.bind(this) }
+       class={ "form-control " + this.props.className } 
+       value={ this.state.data } 
+       onBlur={ e => this.props.child ? null : this.cancel(e) }
        onKeyDown={ this.escapeOrEnter.bind(this) }
        onChange={ e => this.setLocalData(e.target.value) }
        />
@@ -148,7 +149,8 @@ Editable.defaultProps = {
             return (
 <input ref={ input => this.input = input }
        type="text" 
-       class={ "form-control form-control-plaintext " + this.largeClass }
+       class={ "form-control form-control-plaintext " + this.props.className }
+       style={ this.props.style }
        value={ this.props.data }
        onClick={ this.edit.bind(this) }
        onChange={ _ => {} } // onChange needed, readOnly gives default styling
@@ -156,6 +158,12 @@ Editable.defaultProps = {
             )
         }
     }
+}
+
+@observer class RawInput extends Input {
+    get editButtons () { return null; }
+    get title () { return null; }
+    render () { return this.editor; }
 }
 
 @observer class Textarea extends Editable {
@@ -217,11 +225,164 @@ Textarea.defaultProps = {
 }
 
 @observer class MDate extends EditableModal {
+    // For formatting the date into a pretty readable format
+    get prettyDay () {
+        var date = this.state.data;
+        var year = date.getFullYear().toString();
+        var months = ["January", "February", "March", 
+                      "April", "May", "June", "July", 
+                      "August", "September", "October", 
+                      "November", "December"];
+        var month = months[date.getMonth()];
+        var rawDay = date.getDate();
+        // Pick a day suffix, th as default and override for 1st, 2nd, 3rd
+        var daySuffix = "th";
+        if (rawDay <= 3) daySuffix = (["st","nd","rd"])[rawDay-1];
+        var day = rawDay.toString() + daySuffix;
+        
+        if (store.settings.yankeeDates) return `${month} ${day}, ${year}`;
+        else                            return `${day} ${month}, ${year}`;
+    }
+
+    get prettyTime () {
+        var date = this.state.data;
+        var HH = date.getHours().toString().padStart(2, "0");
+        var MM = date.getMinutes().toString().padStart(2, "0");
+        var SS = date.getSeconds().toString().padStart(2, "0");
+
+        return `${HH}:${MM}:${SS}`;
+    }
+
+    get prettyDayOfWeek () {
+        var date = this.state.data;
+        var daysOfWeek = [
+            "Monday", "Tuesday", "Wednesday", "Thursday",
+            "Friday", "Saturday", "Sunday"
+        ]
+        return daysOfWeek[date.getDay()];
+    }
+
+    get prettyDate () {
+        return this.prettyDayOfWeek + " " + this.prettyDay + ", " + this.prettyTime;
+    }
+
+
+    // stores the dom elements of each input
+    inputs = {}
+    // blur all of the elements in sequence
+    blurAll () {
+        var inputs = Object.values(this.inputs);
+        inputs.map(i => i.blur());
+    }
+    // Checks if any of the input dom elements are focused
+    anyFocused () {
+        var inputs = Object.values(this.inputs);
+        return inputs.some(i => i === document.activeElement);
+    }
+    // focus the first element, the year, for when no element has been clicked
+    focusFirst () {
+        this.inputs["year"].focus();
+    }
+
+    // Maps preferred names to Date method parts
+    // e.g. year to FullYear
+    propMap = {
+        "year": "FullYear",
+        "month": "Month",
+        "day": "Date",
+        "hour": "Hours",
+        "minute": "Minutes",
+        "second": "Seconds",
+    }
+
+    //withdraws the property corresponding to the name
+    //sanitizes and pads it correctly
+    getProp (name, length = 2) {
+        var prop = this.propMap[name];
+        var no = this.state.data["get" + prop]();
+
+        if (name === "month") no += 1;
+
+        var str = no.toString().padStart(length, "0");
+        return str;
+    }
+    //writes new value to the relevant prop in the date
+    setProp (name, e) {
+        var prop = this.propMap[name];
+        var newValue = parseInt(e.target.value);
+        
+        if (name === "month") newValue -= 1;
+        
+        var newDate = new Date(this.state.data.getTime());
+        newDate["set" + prop](newValue);
+        this.setLocalData(newDate);
+    }
+
+    boundInput (name, size="sm", props={}) {
+        if (typeof props.className !== "string") props.className = "";
+        props.className += " form-control p-0 text-center no-flex ";
+        props.className += this.props.editing ? "" : " form-control-plaintext ";
+        props.className += size;
+
+        props.onChange = this.setProp.bind(this, name);
+        props.onKeyDown = this.escapeOrEnter.bind(this);
+        props.onClick = this.edit.bind(this);
+
+        props.ref = input => this.inputs[name] = input;
+
+        props.type = "text";
+
+        var length = size === "lg" ? 4 : 2;
+        props.maxLength = length;
+
+        if (this.props.editing) {
+            return (
+<input defaultValue={ this.getProp(name, length) }
+       { ...props }
+       />
+            )
+        } else {
+            return (
+<input value={ this.getProp(name, length) }
+       { ...props }
+       />
+            )
+        }
+    }
+
+    // When leaving, blur all items
+    customSave () { this.blurAll(); }
+    customCancel () { this.blurAll(); }
+
+    // If there is no focused input element, focus on year
+    customEdit () {
+        if (this.anyFocused() === false) this.focusFirst();
+    }
+
     get editor () {
         return (
-null
+<div class="date-editor yes-flex-horizontal input-group">
+    { this.boundInput("year", "lg", { placeholder: "YYYY" }) }
+    <span>/</span>
+    { this.boundInput("month", "md", { placeholder: "MM" }) }
+    <span>/</span>
+    { this.boundInput("day", "md", { placeholder: "DD" }) }
+    <span class="pr-2">,</span>
+    { this.boundInput("hour", "sm", { placeholder: "hh" }) }
+    <span>:</span>
+    { this.boundInput("minute", "sm", { placeholder: "mm" }) }
+    <span>:</span>
+    { this.boundInput("second", "sm", { placeholder: "ss" }) }
+    <span class="mr-4"></span>
+    <div class="yes-flex" onClick={ this.edit.bind(this) }>
+        ({ this.prettyDate })
+    </div>
+</div>
         )
     }
+}
+MDate.defaultProps = {
+    inline: true,
 }
 
 @observer class CVPicker extends EditableModal {
