940abbb3d34aadb088200b548213e1ac2d5eea88
Mikey <31766607+andmikey@users.noreply.github.com>
2018-04-05 13:12:24 +0100

    Namespace all controllers for api/v1 support

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -1,61 +1,65 @@
-class JobsController < ApplicationController
-  before_action :logged_in_user, only: [:create, :edit, :update, :show, :destroy]
-  before_action :correct_user, only: [:edit, :update, :show, :destroy]
-  
-  def show
-    @job = Job.find(params[:id])
-    render json: @job
-  end
-  
-  def new
-    @job = Job.new
-  end
+module Api
+  module V1
+    class JobsController < ApplicationController
+      before_action :logged_in_user, only: [:create, :edit, :update, :show, :destroy]
+      before_action :correct_user, only: [:edit, :update, :show, :destroy]
+      
+      def show
+        @job = Job.find(params[:id])
+        render json: @job
+      end
+      
+      def new
+        @job = Job.new
+      end
 
-  def create
-    @job = Job.new(job_params)
-    if @job.save
-      redirect_to @job
-    else
-      render 'new'
+      def create
+        @job = Job.new(job_params)
+        if @job.save
+          redirect_to @job
+        else
+          render 'new'
+        end
+      end
+
+      def edit
+        @job = Job.find(params[:id])
+      end
+
+      def update
+        @job = Job.find(params[:id])
+        if @job.update(job_params)
+          redirect_to @job
+        else
+          render 'edit'
+        end
+      end
+
+      def destroy
+        @job = Job.find(params[:id])
+        @job.destroy
+      end
+      
+      private
+      def job_params
+        params.requre(:job).permit(:title, :employer, :postlink, :posttext, :applydate, :location,
+                                   :status, :coverletter, :contactdetails, :additionalinfo)
+      end
+
+      def logged_in_user
+        unless logged_in?
+          # flash[:danger] = "Please log in"
+          # redirect_to login_url unless current_user?(@user)
+          render html: "You have not logged in. Please log in.", status: 400
+        end
+      end
+
+      def correct_user
+        @job = current_user.jobs.find_by(id: params[:id])
+        #redirect_to root_url if @job.nil?
+        render html: "Your account does not have access to this resource.", status: 403 if @job.nil?
+      end
+      
     end
   end
-
-  def edit
-    @job = Job.find(params[:id])
-  end
-
-  def update
-    @job = Job.find(params[:id])
-    if @job.update(job_params)
-      redirect_to @job
-    else
-      render 'edit'
-    end
-  end
-
-  def destroy
-    @job = Job.find(params[:id])
-    @job.destroy
-  end
-  
-  private
-  def job_params
-    params.requre(:job).permit(:title, :employer, :postlink, :posttext, :applydate, :location,
-                               :status, :coverletter, :contactdetails, :additionalinfo)
-  end
-
-  def logged_in_user
-    unless logged_in?
-      # flash[:danger] = "Please log in"
-      # redirect_to login_url unless current_user?(@user)
-      render html: "You have not logged in. Please log in.", status: 400
-    end
-  end
-
-  def correct_user
-    @job = current_user.jobs.find_by(id: params[:id])
-    #redirect_to root_url if @job.nil?
-    render html: "Your account does not have access to this resource.", status: 403 if @job.nil?
-  end
-  
 end
