880f71a3d9e378bc349796716e4a01a3524323e9
Mikey <31766607+andmikey@users.noreply.github.com>
2018-04-05 13:09:41 +0100

    Change v1 routing to api/v1 routing

--- config/routes.rb
+++ config/routes.rb
@@ -13,10 +13,10 @@ Rails.application.routes.draw do
   get '/app', to: 'react#index'
 
   root 'static#home'
-
-  namespace :v1 do
-    resources :users
-    resources :jobs
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :jobs
+    end
   end
-  
 end
