be3de5db601a49df03dbcb8a968ee6b20c077300
Mikey <31766607+andmikey@users.noreply.github.com>
2018-04-06 10:33:06 +0100

    Make email uniqueness not case sensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: true
+                    uniqueness: {case_sensitive: false}
 
   has_secure_password
   validates :password, presence: true, length: {minimum: 8}, allow_nil: false
