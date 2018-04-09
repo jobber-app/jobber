4133c1fe8efbac3fea097a44b8fbd082e93c2c22
Mikey <31766607+andmikey@users.noreply.github.com>
2018-04-09 20:13:02 +0100

    Delete commented-out code from jobs

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -48,15 +48,12 @@ module Api
 
       def logged_in_user
         unless logged_in?
-          # flash[:danger] = "Please log in"
-          # redirect_to login_url unless current_user?(@user)
           render html: "You have not logged in. Please log in.", status: 400
         end
       end
 
       def correct_user
         @job = current_user.jobs.find_by(id: params[:id])
-        #redirect_to root_url if @job.nil?
         render html: "Your account does not have access to this resource.", status: 403 if @job.nil?
       end
       
