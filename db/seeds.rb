c415a154f43b52fb1b8db6471e9ea4fc55487f4f
Dylan Thinnes <dylan.j.thinnes@gmail.com>
2018-04-09 20:15:35 +0100

    Make Job seeds comprehensive

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,23 +13,47 @@ user0 = User.create!(name: 'djt', email: 'djt@djt.com', password: 'deejaytee', p
 user1 = User.create!(name: 'ma', email: 'ma@ma.com', password: 'mamamama', password_confirmation: 'mamamama')
 
 # Fill out your jobs here....
-job00 = User.first.jobs.create!({ title: "Analyst",         
-                                 applydate: "2018-05-03 12:01:01", 
-                                 status: 0, 
-                                 #employer: "Beare's Boys",      
-                                 posttext: "Analyze for us!" })
-job01 = User.first.jobs.create!({ title: "Central Manager", 
-                                 applydate: "2018-05-06 12:01:01", 
-                                 status: 1, 
-                                 #employer: "Derek and Sons",    
-                                 posttext: "Manage things!" })
-job10 = User.last.jobs.create!({ title: "Etherium Expert", 
-                                applydate: "2018-05-09 12:01:01", 
-                                status: 2, 
-                                #employer: "Fully Loaded Inc.", 
-                                posttext: "Give us blockchain." })
-job11 = User.last.jobs.create!({ title: "Gardening Guru",  
-                                applydate: "2018-05-30 12:01:01", 
-                                status: 1, 
-                                #employer: "Happy Records",     
-                                posttext: "Demanding shrubberies!" })
+job00 = user0.jobs.create!({ title: "Analyst",         
+                             employer: "Beare's Boys",
+                             postlink: "https://www.google.com",
+                             posttext: "Analyze for us!",
+                             applydate: "2018-05-03 12:01:01",
+                             location: "Edinburgh, United Kingdom",
+                             status: "0", 
+                             coverletter: "...",
+                             contactdetails: "Email a cover letter and cv.",
+                             additionalinfo: "Site gives me deja vu."
+                           })
+job01 = user0.jobs.create!({ title: "Central Manager", 
+                             employer: "Derek and Sons",
+                             postlink: "https://www.google.com",
+                             posttext: "Manage things!", 
+                             applydate: "2018-05-06 12:01:01", 
+                             location: "Edinburgh, United Kingdom",
+                             status: "0", 
+                             coverletter: "...",
+                             contactdetails: "Email a cover letter and cv.",
+                             additionalinfo: "Site gives me deja vu."
+                           })
+job10 = user1.jobs.create!({ title: "Etherium Expert", 
+                             employer: "Fully Loaded Inc.", 
+                             postlink: "https://www.google.com",
+                             posttext: "Give us blockchain.",
+                             applydate: "2018-05-09 12:01:01", 
+                             location: "Edinburgh, United Kingdom",
+                             status: "2", 
+                             coverletter: "...",
+                             contactdetails: "Email a cover letter and cv.",
+                             additionalinfo: "Site gives me deja vu."
+                           })
+job11 = user1.jobs.create!({ title: "Gardening Guru",  
+                             employer: "Happy Records",     
+                             postlink: "https://www.google.com",
+                             posttext: "Demanding shrubberies!",
+                             applydate: "2018-05-30 12:01:01", 
+                             location: "Edinburgh, United Kingdom",
+                             status: "0", 
+                             coverletter: "...",
+                             contactdetails: "Email a cover letter and cv.",
+                             additionalinfo: "Site gives me deja vu."
+                           })
